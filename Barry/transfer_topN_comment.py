bf5c7ac1a5955815d41d872bfa7857afb2316110
barryii <a5020877@gmail.com>
2026-03-31 23:26:09 +0800

    feat: udpate transfer topn comment.py

--- Barry/transfer_topN_comment.py
+++ Barry/transfer_topN_comment.py
@@ -2,9 +2,12 @@ from youtuber_info import Chienseating, HowHowEat
 from database import DBManager
 
 with DBManager().connect_to_db() as conn:
-    with conn.cursor(dictionary=True) as cursor:
+    with conn.cursor() as cursor:
+        N = 1100
+        # 單純以留言數排序的前N名
         sql = """
             SELECT 
+                channel_id,
                 author_id,
                 MAX(author_name) AS author_name,
                 COUNT(comment_id) AS comment_count,
@@ -15,9 +18,35 @@ with DBManager().connect_to_db() as conn:
             ORDER BY comment_count DESC
             LIMIT %s
         """
-
-        cursor.execute(sql, (Chienseating().channel_id, 10))
+        cursor.execute(sql, (Chienseating().channel_id, N))
         results = cursor.fetchall()
-
-        print(results)
+        # print(results)
+        DBManager().save_topN_comment_batch(results)
+        cursor.execute(sql, (HowHowEat().channel_id, N))
+        results = cursor.fetchall()
+        # print(results)
+        DBManager().save_topN_comment_batch(results)
+        
+        # 以總獲得的按讚數排序的前N名
+        sql = """
+            SELECT 
+                channel_id,
+                author_id,
+                MAX(author_name) AS author_name,
+                COUNT(comment_id) AS comment_count,
+                CAST(SUM(like_count) AS UNSIGNED) AS total_likes
+            FROM video_comments
+            WHERE channel_id = %s
+            GROUP BY author_id
+            ORDER BY total_likes DESC, comment_count DESC
+            LIMIT %s
+        """
+        cursor.execute(sql, (Chienseating().channel_id, N))
+        results = cursor.fetchall()
+        # print(results)
+        DBManager().save_topN_comment_batch(results)
+        cursor.execute(sql, (HowHowEat().channel_id, N))
+        results = cursor.fetchall()
+        # print(results)
+        DBManager().save_topN_comment_batch(results)
 
